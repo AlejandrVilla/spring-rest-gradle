2590dc811d6111a7f093f2378abab61764441ded
Avherrera <avillah@unsa.edu.pe>
2024-01-23 22:44:38 -0500

    gradle init

---
+++ src/main/groovy/com/app1/app2/controller/IndexController.groovy
@@ -0,0 +1,13 @@
+package com.app1.app2.controller
+import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestMethod
+
+@RestController
+class indexController{
+    @RequestMapping('/')
+    String index(){
+        'We in the house...'
+    }
+}
+
